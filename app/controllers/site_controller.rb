260edec6be2c917777f088044eded715434dd062
Daniel Hoffens <dhoffens@gmail.com>
2016-09-28 15:00:33 -0400

    added routes to static about pages - purpose, company, contact

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,4 +3,16 @@ class SiteController < ApplicationController
 	def home
 		render :home
 	end
+
+	def purpose
+		render :purpose
+	end
+
+	def company
+		render :company
+	end
+
+	def contact
+		render :contact
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
 root 'site#home'
 devise_for :users
-
 resources :users do
 	resources :jobs do
 		resources :comments 
 	end
 end
-
-
+get '/purpose', to: 'site#purpose'
+get '/company', to: 'site#company'
+get '/contact', to: 'site#contact'
 end
