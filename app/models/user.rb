fa1f1a11ab4d6fb40d54cd087343d4b92edee63b
Daniel Hoffens <dhoffens@gmail.com>
2016-10-04 14:22:16 -0400

    initializing p2p messaging with mailboxer gem. created conversations controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ has_many :jobs
 validates :name, presence: true
 validates :zip, presence: true, numericality: true
 
+acts_as_messageable
+
+# 	def name
+ #      @name = User.name
+ #    end
+
+ #    def mailboxer_email
+ #      @email = User.email
+ #    end
 end
--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -4,10 +4,10 @@ Mailboxer.setup do |config|
   config.uses_emails = true
 
   #Configures the default from for emails sent for Messages and Notifications
-  config.default_from = "no-reply@mailboxer.com"
+  config.default_from = "support@donedeal.co"
 
   #Configures the methods needed by mailboxer
-  config.email_method = :mailboxer_email
+  config.email_method = :email
   config.name_method = :name
 
   #Configures if you use or not a search engine and which one you are using
