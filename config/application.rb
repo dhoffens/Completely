0ecc4f6f19ec2c3ed287ae853fc825dc1cd0f969
Daniel Hoffens <dhoffens@gmail.com>
2016-10-10 12:30:25 -0400

    missed a comma

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Completely
     config.action_mailer.smtp_settings = {
     	:address				=> "smtp.gmail.com",
     	:port 					=> 587,
-        :domain                 => 'gmail.com'
+        :domain                 => 'gmail.com',
     	:user_name				=> ENV['gmail_username'],
     	:password 				=> ENV['gmail_password'],
     	:authentication 		=> "login",
