a866ffb0a9057188f49ccaa398abf9c247a83583
Daniel Hoffens <dhoffens@gmail.com>
2016-10-10 12:47:27 -0400

    meh

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,10 @@ module Completely
     config.action_mailer.smtp_settings = {
     	:address				=> "smtp.gmail.com",
     	:port 					=> 587,
-        :domain                 => 'gmail.com',
+        :domain                 => 'donedealapp.herokuapp.com',
     	:user_name				=> ENV['gmail_username'],
     	:password 				=> ENV['gmail_password'],
-    	:authentication 		=> "login",
+    	:authentication 		=> 'plain',
     	:enable_starttls_auto 	=> true
     }
 
